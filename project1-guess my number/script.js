18c1f508ead911f6c3d041e69a0f8a61fd79a978
Gayasri72 <gayasripethum1@gmail.com>
2024-10-30 11:32:52 +0530

    initial comments

---
+++ project1-guess my number/script.js
@@ -0,0 +1,11 @@
+//document.querySelector('.message');
+console.log(document.querySelector('message'));
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent='correct number!'
+document.querySelector('.number').textContent=25;
+document.querySelector('.score').textContent=35;
+//document.querySelector('.guess').value=45;
+document.querySelector('.guess').value=89;
+console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.score').value);
+
